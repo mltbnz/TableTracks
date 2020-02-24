c9791a83c489533cb6a756851ad9b2e97623ec66
Malte Buenz <malte.buenz@deutschebahn.com>
2020-02-24 11:57:27 +0100

    add project setup

---
+++ Source/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TableTracks
+//
+//  Created by Malte Bünz on 23.02.20.
+//  Copyright © 2020 Malte Bünz. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
